7513506829fd5485065be4abf92a7e68da2088df
Paulo Vitor <paulo.ps84@gmail.com>
2019-01-25 17:40:27 -0200

    ajust removeGeofences

--- src/android/RemoveGeofenceCommand.java
+++ src/android/RemoveGeofenceCommand.java
@@ -6,10 +6,12 @@ import android.util.Log;
 
 import com.google.android.gms.location.GeofencingClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.OnCompleteListener;
 
 import java.util.List;
 
-public class RemoveGeofenceCommand extends AbstractGoogleServiceCommand {
+public class RemoveGeofenceCommand extends AbstractGoogleServiceCommand implements OnCompleteListener<Void> {
     private List<String> geofencesIds;
 
     public RemoveGeofenceCommand(Context context, List<String> geofencesIds) {
@@ -22,22 +24,20 @@ public class RemoveGeofenceCommand extends AbstractGoogleServiceCommand {
         if (geofencesIds != null && geofencesIds.size() > 0) {
             logger.log(Log.DEBUG, "Removing geofences...");
             GeofencingClient geofencingClient = LocationServices.getGeofencingClient(this.context);
-            geofencingClient.removeGeofences(geofencesIds).addOnSuccessListener(this, new OnSuccessListener<Void>() {
-                @Override
-                public void onSuccess(Void aVoid) {
-                    logger.log(Log.DEBUG, "Geofences successfully removed");
-                    CommandExecuted();
-                }
-            }).addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception e) {
-                    String message = "Removing geofences failed - " + e.getMessage();
-                    logger.log(Log.ERROR, message);
-                    CommandExecuted(new Error(message));
-                }
-            });
+            geofencingClient.removeGeofences(geofencesIds).addOnCompleteListener(this);
         } else {
             logger.log(Log.DEBUG, "Tried to remove Geofences when there were none");
+            CommandExecuted();
+        }
+    }
+
+    @Override
+    public void onComplete(@NonNull Task<Void> task) {
+        if (task.isSuccessful()) {
+            logger.log(Log.DEBUG, "Geofences successfully removed");
+            CommandExecuted();
+        } else {
+            logger.log(Log.DEBUG, "Geofences error on remove");
             CommandExecuted();
         }
     }
